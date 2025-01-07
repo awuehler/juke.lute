1f34631decf875a8a6d9c43ec8c6378e85538a85
Andy W... <andy.wuehler@gmail.com>
2025-01-07 14:21:28 -0800

    fix logic problems and update comments

--- my.tools/jukebox.ps1
+++ my.tools/jukebox.ps1
@@ -1,6 +1,7 @@
 <#
 .SYNOPSIS
-    Script to play a random list of *.abc files using AbcPlayer or Maestro.
+    Script to play a random list of *.abc files using AbcPlayer or Maestro
+    using the lute instrument (default).
 
 .DESCRIPTION
     This script is used to open an ABC player to load a selected melody file
@@ -14,6 +15,12 @@
 
         e.g. ".\jukebox.ps1"
     
+    The default instrument (i.e. "X: 1" inside the *.abc file) is mapped to
+    the "Lute Of Ages" instrument in both the AbcPlayer and Maestro programs.
+        - Because the other %% lines added by Maestro have been removed
+          from the solo instrument *.abc files in the juke.lute folder
+          which were used to identify the other types of instruments
+
     WARNING:
     If PowerShell script execution is blocked by your MSW security policy,
     then try the following steps to allow the execution of *.ps1 scripts:
@@ -41,6 +48,8 @@
             - set default pause between each *.abc melody file
         - Track previous melodies to skip
             - to avoid repeats within a given folder of tunes
+        - Remove duplicate code and rewrite back-to-back duplicate checking
+        - Add pick by metadata options (i.e. keys, beats, keywords, so on)
         - ...
 #>
 
@@ -58,7 +67,7 @@ $music_editor = """C:\Program Files (x86)\Maestro\Maestro.exe"""
 ################## End-User Modifications (if needed) ##################
 ########################################################################
 
-# Build the collection of melody files.
+# Build an array list of melody files.
 try {
     $music_collection = ( Get-ChildItem -Path $music_abc_path -Recurse -File | Select-Object -Property FullName )
 }
@@ -67,8 +76,7 @@ catch {
     Write-Host $_
 }
 
-# Pick a random melody to test and verify.
-# TODO: convert to function
+# Seed an initial random pick for test and verify.
 try {
     $global:music_random = ( $music_collection | Get-Random | Select-Object -ExpandProperty FullName )
 }
@@ -77,14 +85,14 @@ catch {
     Write-Host $_
 }
 
-# Create placeholder array.
+# Build an array list of sub folders within the juke box (assumes flat directory structure).
 $folder_array = @()
-# Add juke folders into array (assumes flat directory structure).
+# Find each (unique) folder across all *.abc files.
 foreach ($melody in $music_collection) {
     $file_parent = Split-Path -Path "$melody" -Parent
     $file_folder = Split-Path -Path "$file_parent" -Leaf
-    # Unique folders only.
-    if ($folder_array -notcontains $file_folder) {
+    # Test to add unique folders only.
+    if ($folder_array -NOTcontains $file_folder) {
         $folder_array += $file_folder
     }
 }
@@ -92,8 +100,23 @@ $folder_array += "ALL"
 
 <#
 .SYNOPSIS
-    Function: Select type of application to play *.abc files
+    Pick a random melody to test and verify (assumes Get-Random is not pseudo random i.e. biased).
 #>
+function ProbabilityPick {
+    param (
+        $abc_list
+    )
+
+    try {
+        $abc_pick = ( $abc_list | Get-Random | Select-Object -ExpandProperty FullName )
+    }
+    catch {
+        Write-Host "An error occurred to select a melody file..."
+        Write-Host $_
+    }
+
+    return $abc_pick
+}
 function PlayMelody {
     # One parameter to pass into function.
     param (
@@ -154,12 +177,13 @@ function FormatTimeToSecond {
 function NextMelody {
     # Condition checking (as per folder selection).
     if ($folder_array[[Int]$folder_pick] -match 'ALL') {
-        $random_melody = ( Get-Random -InputObject $music_collection | Select-Object -ExpandProperty FullName )
-        # Check for back to back duplicates. TODO: re-factor to remove duplicate code
+        $random_melody = ProbabilityPick $music_collection
+        # Check for back to back duplicates.
+        # TODO: re-factor to remove duplicate code
         if ($random_melody -eq $global:music_random) {
             # NOTE: Back to back duplicates can still occur, BTW.
             # This kluge is a basic hack to lower its probability.
-            $random_melody = ( Get-Random -InputObject $music_collection | Select-Object -ExpandProperty FullName )
+            $random_melody = ProbabilityPick $music_collection
             $global:music_random = $random_melody
         } else {
             $global:music_random = $random_melody
@@ -168,12 +192,13 @@ function NextMelody {
     else {
         # Repeat the random melody pick until a tune from target folder is returned.
         do {
-            $random_melody = ( Get-Random -InputObject $music_collection | Select-Object -ExpandProperty FullName )
-            # Check for back to back duplicates. TODO: re-factor to remove duplicate code
+            $random_melody = ProbabilityPick $music_collection
+            # Check for back to back duplicates.
+            # TODO: re-factor to remove duplicate code
             if ($random_melody -eq $global:music_random) {
                 # NOTE: Back to back duplicates can still occur, BTW.
                 # This kluge is a basic hack to lower its probability.
-                $random_melody = ( Get-Random -InputObject $music_collection | Select-Object -ExpandProperty FullName )
+                $random_melody = ProbabilityPick $music_collection
                 $global:music_random = $random_melody
             } else {
                 $global:music_random = $random_melody
@@ -222,8 +247,9 @@ do {
     # Capture user selection for re-use.
     $player_type = Read-Host "Please enter which player to use (default is AbcPlayer)"
     # Test user input (none vs. number) and assign default when null.
-    if (-not $player_type) {$player_type = $def_player}
-} while (-not ($player_type -match '^\d?1|2'))
+    if (-NOT $player_type) {$player_type = $def_player}
+
+} while (-NOT ($player_type -match '^\d?1|2'))
 
 <#
 .SYNOPSIS
@@ -242,9 +268,9 @@ do {
     # Capture user selection, if any index number is entered
     $folder_pick = Read-Host ("`nPlease enter the number for which folder to use (default is [{0}]ALL)" -f $def_folder)
     # Test user input (none vs. number) and assign default when null.
-    if (-not $folder_pick) {$folder_pick = $def_folder}
+    if (-NOT $folder_pick) {$folder_pick = $def_folder}
 
-} while (((-not ($folder_pick -match '^\d+$')) -AND ($folder_pick -le "$folder_array.Length")))
+} while (-NOT ( ($folder_pick -match '^\d+$') -AND ($folder_pick -le $folder_array.Length - 1) ) )
 
 <#
 .SYNOPSIS
